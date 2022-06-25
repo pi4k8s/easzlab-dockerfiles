11c8f13e74f057b0a9a304696cc755b66caac470
fastjrun <fastjrun@139.com>
2022-06-25 17:54:56 +0800

    构建easzlab-ansible

---
+++ ansible/docker-lite.groovy
@@ -0,0 +1,12 @@
+node {
+    stage('git chekout') {
+        git branch: "master", url: 'https://github.com/pi4k8s/easzlab-dockerfiles.git'
+    }
+    stage('parallel docker build') {
+        sh "cd ansible && docker build . -t pi4k8s/easzlab-ansible:2.10.6-lite"
+        sh "docker push pi4k8s/easzlab-ansible:2.10.6-lite"
+    }
+    stage('cleanWs'){
+        cleanWs()
+    }
+}
