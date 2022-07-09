2776e1dafd1d49f28dd2cfe1da61606a8cf9d221
fastjrun <fastjrun@139.com>
2022-07-09 15:00:08 +0800

    升级kubeasz为3.3.1

--- kubeasz/docker.groovy
+++ kubeasz/docker.groovy
@@ -3,8 +3,8 @@ node {
         git branch: "master", url: 'https://github.com/pi4k8s/easzlab-dockerfiles.git'
     }
     stage('docker build & push') {
-        sh "cd kubasz && docker build . -t pi4k8s/easzlab-kubasz:3.3.1"
-        sh "docker push pi4k8s/easzlab-kubasz:3.3.1"
+        sh "cd kubeasz && docker build . -t pi4k8s/easzlab-kubeasz:3.3.1"
+        sh "docker push pi4k8s/easzlab-kubeasz:3.3.1"
     }
     stage('cleanWs'){
         cleanWs()
